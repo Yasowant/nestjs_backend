c0b4c6e23ce741656308f804447afd56a6495443
Yasowant <yasowant1998@gmail.com>
2025-02-14 21:52:00 +0530

    crud operation for property complited

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,18 +1,42 @@
-import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreatePropertyDto } from './dto/createProperty.dto';
 import { PropertyService } from './property.service';
+import { updatePropertyDto } from './dto/updateProperty.dto';
 
 @Controller('property')
 export class PropertyController {
   constructor(private propertyService: PropertyService) {}
   @Get()
-  findAll() {}
+  findAll() {
+    return this.propertyService.findAll();
+  }
   @Get(':id')
-  findOne() {}
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.propertyService.findOne(id);
+  }
   @Post()
   create(@Body() dto: CreatePropertyDto) {
     return this.propertyService.create(dto);
   }
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: updatePropertyDto,
+  ) {
+    return this.propertyService.update(id, dto);
+  }
+
   @Delete(':id')
-  delete() {}
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.propertyService.delete(id);
+  }
 }
--- src/property/property.service.ts
+++ src/property/property.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Property } from 'src/entities/property.entity';
 import { CreatePropertyDto } from './dto/createProperty.dto';
+import { updatePropertyDto } from './dto/updateProperty.dto';
 
 @Injectable()
 export class PropertyService {
@@ -10,11 +11,29 @@ export class PropertyService {
     @InjectRepository(Property)
     private readonly propertyRepo: Repository<Property>,
   ) {}
-  async findAll() {}
-  async findOne() {}
+  async findAll() {
+    return await this.propertyRepo.find();
+  }
+  async findOne(id: number) {
+    const property = await this.propertyRepo.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!property) {
+      throw new NotFoundException();
+    }
+    return property;
+  }
   async create(dto: CreatePropertyDto) {
     return await this.propertyRepo.save(dto);
   }
-  async update() {}
-  async delete() {}
+  async update(id: number, dto: updatePropertyDto) {
+    return await this.propertyRepo.update({ id }, dto);
+  }
+  async delete(id: number) {
+    return await this.propertyRepo.delete({
+      id,
+    });
+  }
 }
